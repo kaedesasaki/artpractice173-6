6b75fadb1506705def0a3db4308e6efffd1d9bcc
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-14 11:13:34 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+class Fox {
+
+    constructor(){
+      this.name = 'kiki';
+      this.home = 'Star Cloud';
+      this.friends = 'lala';
+      this.seeking = 'star';
+      this.route = 'blue/stat/road';
+     
+  
+    }
+  
+
+
+     laugh(){
+        if(getStar) {
+            say (Yay);
+        } 
+    }
+
+    cry(){
+        //cry when attacked
+    }
+
+    jump(){
+
+    }
+
+    getStar(){
+    }
+
+
+
+}
